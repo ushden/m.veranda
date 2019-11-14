aa3b4690029410152921cf451e0a18e8a857496f
ushden <ushden48@gmail.com>
2019-11-14 23:13:33 +0200

    Veranda 2.0

--- js/main.js
+++ js/main.js
@@ -1,17 +1,37 @@
 $(function(){
-    $('.slider').slick({
-        dots: false,
-        infinite: true,
-        speed: 500,
-        fade: true,
-        cssEase: 'linear',
-        arrows: false,
+    $('.burger').click(function(e){
+        e.preventDefault
+        $('.burger').toggleClass('burger__active');
+        $('.nav__menu-list').toggleClass('nav__menu-active');
+        $('.nav__logo').toggleClass('nav__logo-none');
+        $('.header__info').toggleClass('header__info-hidden');
+        $('.arrow').toggleClass('arrow__hidden');
     });
-    
-    $('.menu__active').click(function(e){
-        e.preventDefault ();
-        $('.menu_active').removeAttr('href');
-        $('.menu__active').toggleClass('menu__click');
-        $('.menu').toggleClass('menu__show')
-    });  
+    $('.nav__menu-list, .nav__menu-link').click(function(){
+        $('.burger').removeClass('burger__active');
+        $('.nav__menu-list').removeClass('nav__menu-active');
+        $('.nav__logo').removeClass('nav__logo-none');
+        $('.header__info').removeClass('header__info-hidden');
+        $('.arrow').removeClass('arrow__hidden');
+    });
+
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 30) {
+            $('#header__slick').addClass('header__slick-show');
+        }
+        else {
+            $('#header__slick').removeClass('header__slick-show');
+        }
+    });    
+    $('.slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        cssEase: 'liner',
+        dots: false,
+        arrows: false
+      });
 });
