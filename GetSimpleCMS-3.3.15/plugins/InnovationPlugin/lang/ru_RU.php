9c3824c5d17b4358876b3d1b92536b6f6132da52
ushden <ushden48@gmail.com>
2019-12-13 19:29:00 +0200

    test new adminpanel

---
+++ GetSimpleCMS-3.3.15/plugins/InnovationPlugin/lang/ru_RU.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Russian Language File for Innovation Theme Plugin
+ * Translation Oleg Svetlov http://getsimplethemes.ru/
+ *
+ * Date:				19 Feb 2012
+ * Revision:		
+ * Version:			GetSimple 3.0
+ * Traductors: 	Chris Cagle 
+ *
+ * @package GetSimple
+ * @subpackage Language
+ */
+ 
+$i18n = array (
+	
+	"INNOVATION_TITLE"  =>  "Настройки социальных сетей",
+	"INNOVATION_DESC"   =>  "Настройки по умлочанию для GetSimple темы: Innovation",
+	"FACEBOOK-F_ERROR"	=>	"Facebook - ссылка не верна.",
+	"GOOGLE-PLUS_ERROR"	=>	"Google+ - ссылка не верна.",
+	"TWITTER_ERROR"		=>	"Twitter - ссылка не верна.",
+	"LINKEDIN_ERROR"	=>	"LinkedIn - ссылка не верна.",
+	"MYSPACE_ERROR"	    =>	"Myspace - ссылка не верна.",
+	"DIGG_ERROR"	    =>	"Digg - ссылка не верна.",
+	"DRIBBBLE_ERROR"    =>  "Dribbble - ссылка не верна.",
+	"DELICIOUS_ERROR"	=>	"Delicious - ссылка не верна.",
+	"LIVEJOURNAL_ERROR"	=>	"Livejournal - ссылка не верна.",
+	"BLOGGER_ERROR"	    =>	"Blogger - ссылка не верна.",
+	"VK_ERROR"	        =>	"Вконтакте - ссылка не верна.",
+	"ODNOKLASSNIKI_ERROR" =>"Одноклассники - ссылка не верна.",
+	"YOUTUBE_ERROR"	    =>	"Youtube - ссылка не верна.",
+	"FLICKR_ERROR"	    =>	"Flickr - ссылка не верна.",
+	"PINTEREST_ERROR"	=>	"Pinterest - ссылка не верна.",
+	"TUMBLR_ERROR"      =>  "Tumblr - ссылка не верна.",
+	"INSTAGRAM_ERROR"   =>  "Instagram - ссылка не верна.",
+	"VIMEO-SQUARE_ERROR"=>  "Vimeo - ссылка не верна.",
+	"GITHUB_ERROR"      =>  "Github - ссылка не верна.",
+	"RSS_ERROR"	        =>	"RSS - ссылка не верна.",
+	
+	"FACEBOOK_URL"	=>	"Facebook ссылка",
+	"GOOGLE-PLUS_URL"   =>	"Google+ ссылка",
+	"TWITTER_URL"		=>	"Twitter ссылка",
+	"VK_URL"		    =>	"Вконтакте ссылка",
+	"ODNOKLASSNIKI_URL" =>	"Одноклассники ссылка",
+	"LINKEDIN_URL"		=>	"LinkedIn ссылка",
+	"MYSPACE_URL"		=>	"Myspace ссылка (нет иконки в шрифте Awesome)",
+	"DIGG_URL"		    =>	"Digg ссылка",
+	"DRIBBBLE_URL"      =>  "Dribbble ссылка",
+	"DELICIOUS_URL"		=>	"Delicious ссылка",
+	"LIVEJOURNAL_URL"	=>	"Livejournal ссылка (нет иконки в шрифте Awesome)",
+	"BLOGGER_URL"		=>	"Blogger ссылка (нет иконки в шрифте Awesome)",
+	"YOUTUBE_URL"		=>	"Youtube ссылка",
+    "FLICKR_URL"		=>	"Flickr ссылка",
+	"PINTEREST_URL"		=>	"Pinterest ссылка",
+	"TUMBLR_URL"        =>  "Tumblr ссылка",
+	"INSTAGRAM_URL"     =>  "Instagram ссылка",
+	"VIMEO_URL"  =>  "Vimeo ссылка",
+	"GITHUB_URL"        =>  "Github ссылка",
+	"RSS_URL"		    =>	"RSS ссылка (http://site_url/rss/?news)"
+);
